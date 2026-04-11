c66ca6154e6ddd881879422885695535171f0a3f
chaitanya verma <vchaitanya200511@gmail.com>
2026-04-12 00:17:51 +0530

    Add cart UpdateItemCart feature

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -109,3 +109,57 @@ export const removeFromCart = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const updateCartItem = async (req,res) => {
+    try {
+        
+     const userId = req.user._id;
+     const {productId,quantity} = req.body;
+
+    const cart = await cartModel.findOne({user: userId});
+
+    if(!cart){
+        return res.status(404).json({message:"Cart not found"});
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (item) => item.product.toString() === productId
+    );
+
+    if (itemIndex === -1) {
+      return res.status(404).json({ message: "Product not in cart" });
+    }
+
+    // 🔥 update quantity
+    if (quantity <= 0) {
+      // remove item
+      cart.items.splice(itemIndex, 1);
+    } else {
+      cart.items[itemIndex].quantity = quantity;
+    }
+
+    // recalculate totals
+    cart.totalItems = cart.items.reduce(
+      (acc, item) => acc + item.quantity,
+      0
+    );
+
+    cart.totalPrice = cart.items.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
+
+    await cart.save();
+
+    res.status(200).json({
+      message: "Cart updated",
+      cart,
+    });
+
+    } catch (error) {
+        console.error("Update cart error:",error);
+        return res.status(500).json({
+            message:"Server Error"
+        });
+    }
+};
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { addToCart, getCart, removeFromCart } from "../controllers/cart.controller.js";
+import { addToCart, getCart, removeFromCart, updateCartItem } from "../controllers/cart.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/",protect,addToCart);
 router.get("/",protect,getCart);
 router.delete("/:id",protect,removeFromCart);
+router.put("/",protect,updateCartItem);
 
 export default router;
